95ed2ed3db0fee1b16c276b0635e797eabb36c1e
yasmeinelkenany <yasmeinelkenany@gmail.com>
2022-11-30 16:23:21 +0200

    Edit index.hmtl and styles.css and script.js

--- script.js
+++ script.js
@@ -3,54 +3,84 @@ let sketch = document.getElementById("sketch");
 let squareRange = document.getElementById("range");
 let rangeNum = document.getElementById("range-num");
 let newDivs;
+let squaresStyling;
 
-//change sketch squares range
 function changeRange() {
+    squaresStyling = `background-color: #fff; width:calc(100% /${range.value});
+height:calc(100% /${squareRange.value});
+border: ${1 / squareRange.value}px solid #a9a8a8;
+border-bottom: none;
+border-right: none;
+cursor: crosshair`;
     if (squareRange.value > 0) {
         sketch.innerHTML = "";
+        color.value = "#ffffff"
         for (let i = 0; i < squareRange.value * squareRange.value; i++) {
             rangeNum.textContent = range.value;
             newDivs = document.createElement("div");
             newDivs.className = "newClassList";
-            newDivs.style.cssText = `width:calc(100% /${squareRange.value});
-             height:calc(100% /${squareRange.value});
-             background-color: #fff;
-             border: ${1 / squareRange.value}px solid #a9a8a8;
-             border-bottom: none;
-             border-right:none`;
+            newDivs.style.cssText = `${squaresStyling}`;
             sketch.appendChild(newDivs);
         }
-        for (sketchChild of sketch.children) {
-            removeEventListener("mouseover", coloring, true)
-            sketchChild.addEventListener("mouseover", coloring);
-        }
-
     }
-    return newDivs;
+    return squaresStyling, newDivs;
 }
-changeRange()
-squareRange.addEventListener('input', changeRange);
-
-//coloring 
+changeRange();
+squareRange.addEventListener("input", changeRange);
+//coloring
 let colorPalet = document.getElementById("color");
 let paletOne = document.getElementById("palet-one");
 let paletTwo = document.getElementById("palet-two");
 let eraser = document.getElementById("eraser");
 let clear = document.getElementById("clear");
+let paletOneColors = ["#a9def9", "#e4c1f9", "#d0f4de", "#fcf6bd", "#ff99c8", "#0077b6", "#b5e48c", "#7b2cbf", "#ffb3c6"];
+let paletTwoColors = ["#fe6d73", "#000000", "#ffcb77", "#17c3b2", "#227c9d", "#9b5de5", "#ff9770", "#00bbf9", "#80ed99"];
 
-let paletOneColors = ['#e4c1f9', '#a9def9', '#d0f4de', '#fcf6bd', '#ff99c8'];
-let paletTwoColors = ['#fe6d73', '#fef9ef', '#ffcb77', '#17c3b2', '#227c9d'];
+//coloring with color input value
+colorPalet.addEventListener("click", function () {
+    for (const sketChild of sketch.children) {
+        sketChild.addEventListener("mousemove", function () {
+            sketChild.style.cssText = `${squaresStyling}; background-color: ${color.value};`;
+        });
+    }
+});
 
-
-function coloring(event) {
-    event.target.style.cssText = `background-color: ${colorPalet.value}; width:calc(100% /${range.value});
-        height:calc(100% /${squareRange.value});
-        border: ${1 / squareRange.value}px solid #a9a8a8;
-        border-bottom: none;
-        border-right:none`;
+//choosing randomly from palets
+function randomColor(palet) {
+    return Math.floor(Math.random() * palet.length);
 }
+//coloring with palet one colors
+paletOne.addEventListener("click", function () {
+    for (const sketChild of sketch.children) {
+        sketChild.addEventListener("mousemove", function () {
+            let chosenColor1 = randomColor(paletOneColors);
+            sketChild.style.cssText = `${squaresStyling}; background-color: ${paletOneColors[chosenColor1]};`;
+        });
+    }
+});
 
+//coloring with palet two colors
+paletTwo.addEventListener("click", function () {
+    for (const sketChild of sketch.children) {
+        sketChild.addEventListener("mousemove", function () {
+            let chosenColor2 = randomColor(paletTwoColors);
+            sketChild.style.cssText = `${squaresStyling}; background-color: ${paletTwoColors[chosenColor2]};`;
+        });
+    }
+});
 
-let choosingColor = Math.floor(Math.random() * paletOneColors.length);
-console.log(choosingColor);
+//remove colors by mouseovering the grid items
+eraser.addEventListener("click", function () {
+    for (const sketChild of sketch.children) {
+        sketChild.addEventListener("mousemove", function () {
+            sketChild.style.cssText = `background-color: #fff; ${squaresStyling}; cursor:pointer`;
+        });
+    }
+});
 
+//clear all sketch
+clear.addEventListener("click", function () {
+    for (const sketChild of sketch.children) {
+        sketChild.style.cssText = squaresStyling;
+    }
+});
